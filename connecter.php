550cdb0d3188716bffec80766d6d99c5cb3ca544
Dev-geek1 <geek.dev.dev@gmail.com>
2022-06-09 08:32:20 +0000

    crud de la table camion

--- connecter.php
+++ connecter.php
@@ -1,6 +1,6 @@
 <?php 
 try{
-	$bdd = new PDO("mysql:host=localhost; dbname=gestiontransport", "root", "");
+	$bdd = new PDO("mysql:host=localhost; dbname=gestiondetransport", "root", "");
 	$bdd -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	//echo "Connexion établie";
 }catch (Exception $e){
