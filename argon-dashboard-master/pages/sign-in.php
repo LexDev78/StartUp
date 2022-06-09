525871b81449b6998961f25f7216214d5785dc9d
Labati Gassamba <labatigassamba72@gmail.com>
2022-06-09 09:13:46 +0000

    connexion à la base de donnée

--- argon-dashboard-master/pages/sign-in.php
+++ argon-dashboard-master/pages/sign-in.php
@@ -50,12 +50,6 @@
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link me-2" href="../pages/profile.php">
-                    <i class="fa fa-user opacity-6 text-dark me-1"></i>
-                    Profile
-                  </a>
-                </li>
-                <li class="nav-item">
                   <a class="nav-link me-2" href="../pages/sign-up.php">
                     <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
                     Sign Up
@@ -68,11 +62,7 @@
                   </a>
                 </li>
               </ul>
-              <ul class="navbar-nav d-lg-block d-none">
-                <li class="nav-item">
-                  <a href="https://www.creative-tim.com/product/argon-dashboard" class="btn btn-sm mb-0 me-1 btn-primary">Free Download</a>
-                </li>
-              </ul>
+              
             </div>
           </div>
         </nav>
